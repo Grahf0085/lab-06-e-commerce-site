55248ae4eee1ebd91875887ea71a6cbbd53be59a
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-31 21:01:19 -0400

    add script tag to html because was silly and forgot it

--- basket/basket.js
+++ basket/basket.js
@@ -2,9 +2,10 @@ import { basket } from '../data/basket.js';
 import { ferns } from '../ferns.js';
 import { renderLineItems } from '../basket/render-line-items.js';
 import { findById } from '../utils.js';
+// import { calcOrderTotal } from '../utils.js';
 
 const table = document.querySelector('table');
-
+console.log(table);
 for (let basketItem of basket) {
 
     const matchingFern = findById(basketItem.id, ferns);
